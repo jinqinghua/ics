4350188f35e39ee218b0f30097757104abc8ac5a
Kim Jin (qinjin) <qinjin@cisco.com>
2025-03-21 20:51:12 +0800

    Optimize code

--- src/main/java/kim/ics/generator/example/TymeShowCase.java
+++ src/main/java/kim/ics/generator/example/TymeShowCase.java
@@ -10,15 +10,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TymeShowCase {
 
-    public static SolarDay FIRST_SOLAR_DAY = SolarDay.fromYmd(2025, 1, 1);
-    public static LunarDay FIRST_LUNAR_DAY = LunarDay.fromYmd(2025, 1, 1);
-    public static int OFFSET = 365;
-
     public static void main(String[] args) {
-        printLegalHoliday(FIRST_SOLAR_DAY, OFFSET);
-        printSolarTerm(FIRST_SOLAR_DAY, OFFSET);
-        printSolarFestival(FIRST_SOLAR_DAY, OFFSET);
-        printLunarFestival(FIRST_LUNAR_DAY, OFFSET);
+        SolarDay firstSolarDay = SolarDay.fromYmd(2025, 1, 1);
+        LunarDay firstLunarDay = LunarDay.fromYmd(2025, 1, 1);
+        int offset = 365;
+
+        printLegalHoliday(firstSolarDay, offset);
+        printSolarTerm(firstSolarDay, offset);
+        printSolarFestival(firstSolarDay, offset);
+        printLunarFestival(firstLunarDay, offset);
     }
 
     /**
