00bceb1dfe21d19f3ac0c030131ffb83b4541941
Kim Jin (qinjin) <qinjin@cisco.com>
2026-04-10 09:42:24 +0800

    1. Fix DTSTAMP to DTSTART, 2. Merge continues vent to one.

--- src/main/java/kim/ics/calenar/MyCalendar.java
+++ src/main/java/kim/ics/calenar/MyCalendar.java
@@ -3,15 +3,15 @@ package kim.ics.calenar;
 import lombok.Getter;
 import lombok.NonNull;
 import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Property;
 import net.fortuna.ical4j.model.component.VEvent;
-import net.fortuna.ical4j.model.property.Method;
-import net.fortuna.ical4j.model.property.ProdId;
-import net.fortuna.ical4j.model.property.XProperty;
+import net.fortuna.ical4j.model.property.*;
 import net.fortuna.ical4j.model.property.immutable.ImmutableCalScale;
 import net.fortuna.ical4j.model.property.immutable.ImmutableVersion;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.time.*;
+import java.time.temporal.Temporal;
+import java.util.*;
 
 @Getter
 public class MyCalendar extends Calendar {
@@ -39,7 +39,84 @@ public class MyCalendar extends Calendar {
     }
 
     public void add(@NonNull Collection<VEvent> vEvents) {
-        vEvents.forEach(this::add);
+        mergeConsecutiveEvents(vEvents).forEach(vEvent -> {
+            applyStableDtStamp(vEvent);
+            this.add(vEvent);
+        });
+    }
+
+    private static List<VEvent> mergeConsecutiveEvents(Collection<VEvent> vEvents) {
+        Map<String, List<VEvent>> eventsBySummary = new HashMap<>();
+        List<VEvent> passthroughEvents = new ArrayList<>();
+
+        vEvents.forEach(vEvent -> {
+            if (toLocalDate(vEvent, Property.DTSTART).isPresent() && toLocalDate(vEvent, Property.DTEND).isPresent()) {
+                String summary = vEvent.getProperty(Property.SUMMARY).map(Property::getValue).orElse("");
+                eventsBySummary.computeIfAbsent(summary, ignored -> new ArrayList<>()).add(vEvent);
+            } else {
+                passthroughEvents.add(vEvent);
+            }
+        });
+
+        List<VEvent> mergedEvents = new ArrayList<>();
+        eventsBySummary.values().forEach(summaryEvents -> {
+            summaryEvents.sort(Comparator.comparing(event -> toLocalDate(event, Property.DTSTART).orElseThrow()));
+
+            VEvent current = summaryEvents.getFirst();
+            for (int i = 1; i < summaryEvents.size(); i++) {
+                VEvent next = summaryEvents.get(i);
+                LocalDate currentEnd = toLocalDate(current, Property.DTEND).orElseThrow();
+                LocalDate nextStart = toLocalDate(next, Property.DTSTART).orElseThrow();
+
+                if (currentEnd.equals(nextStart)) {
+                    LocalDate nextEnd = toLocalDate(next, Property.DTEND).orElseThrow();
+                    current.replace(new DtEnd<>(nextEnd));
+                } else {
+                    mergedEvents.add(current);
+                    current = next;
+                }
+            }
+            mergedEvents.add(current);
+        });
+
+        mergedEvents.addAll(passthroughEvents);
+        mergedEvents.sort(Comparator
+                .comparing((VEvent event) -> toLocalDate(event, Property.DTSTART).orElse(LocalDate.MAX))
+                .thenComparing(event -> event.getProperty(Property.SUMMARY).map(Property::getValue).orElse("")));
+        return mergedEvents;
+    }
+
+    private static void applyStableDtStamp(VEvent vEvent) {
+        vEvent.<DateProperty<? extends Temporal>>getProperty(Property.DTSTART)
+                .map(DateProperty::getDate)
+                .map(MyCalendar::toInstant)
+                .ifPresent(instant -> vEvent.replace(new DtStamp(instant)));
+    }
+
+    private static Instant toInstant(Temporal startDate) {
+        if (startDate instanceof Instant instant) {
+            return instant;
+        }
+        if (startDate instanceof LocalDate localDate) {
+            return localDate.atStartOfDay().toInstant(ZoneOffset.UTC);
+        }
+        if (startDate instanceof LocalDateTime localDateTime) {
+            return localDateTime.toInstant(ZoneOffset.UTC);
+        }
+        if (startDate instanceof OffsetDateTime offsetDateTime) {
+            return offsetDateTime.toInstant();
+        }
+        if (startDate instanceof ZonedDateTime zonedDateTime) {
+            return zonedDateTime.toInstant();
+        }
+        return Instant.from(startDate);
+    }
+
+    private static Optional<LocalDate> toLocalDate(VEvent vEvent, String propertyName) {
+        return vEvent.<DateProperty<? extends Temporal>>getProperty(propertyName)
+                .map(DateProperty::getDate)
+                .filter(LocalDate.class::isInstance)
+                .map(LocalDate.class::cast);
     }
 
 }
